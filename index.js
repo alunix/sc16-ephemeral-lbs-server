db1e14f92d22f2912218e1b726b5ffe7a3f7ebe9
sauer2 <alfred_z@web.de>
2016-05-03 21:58:12 +0200

    implement first version of post requests

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var msgdb = "messages";
 
 var express = require('express');
 var nano = require('nano')(dbserver + ':' + dbport);
+var bodyParser = require("body-parser");
 
 /*
  * We set up the server to serve static files from /public,
@@ -24,6 +25,10 @@ server.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     next();
 });
+
+/* This configures body-parser. */
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
 
 /* Handle basic requests... */
 server.get('/api/zones', function(req, res) {
@@ -49,6 +54,26 @@ server.get('/api/messages', function(req, res) {
         else {
             res.status(400).send('Database error! Couldn\'t fetch messages.');
         }
+    });
+});
+
+server.post('/api/addmessage', function(req, res) {
+    var messages = nano.use(msgdb);
+    messages.insert({
+        title: req.body.title,
+        body: req.body.body,
+        category: req.body.category,
+        timestamp: req.body.timestamp,
+        expirationdate: req.body.expirationdate,
+        zonename: req.body.zonename
+    });
+});
+
+server.post('/api/addzone', function(req, res) {
+    var zones = nano.use(zonedb);
+    zones.insert({
+        name: req.body.name,
+        geometry: req.body.geometry
     });
 });
 
