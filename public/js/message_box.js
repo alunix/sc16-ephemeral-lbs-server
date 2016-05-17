a1a5a91ab9eb2d624a8c1ca7824b4568f0f5119a
JanVan01 <j_vanz01@uni-muenster.de>
2016-05-18 00:34:41 +0200

    add basic functionality to display messages form database

--- public/js/message_box.js
+++ public/js/message_box.js
@@ -1,9 +1,18 @@
 // Get message box
 function update(){
-  
- document.forms['message_form']["messagearea"].innerHTML = 'Changed Text';
- 
-     
+  $.ajax({
+     url: 'http://localhost:8080/api/messages?zone=1',
+     type: 'get',
+     dataType: 'json',
+     success: function(data) {
+       for(var i = 0; i< data['Messages'].length; i++) {
+         console.log(data['Messages'][i]);
+         $('#messagearea').append('<div class="message">'+data['Messages'][i]['Body']['Title']+'</div>');
+       }
+     }
+  });
+
+
+
+
 }
-
-
--- public/js/requests.js
+++
@@ -1,9 +0,0 @@
-$.ajax({
-   url: 'http://localhost:5984/',
-   type: 'get',
-   dataType: 'jsonp',
-   success: function(data) {
-      alert(data.couchdb);
-      alert(data.version);
-   }
-});
