11e067c52f3da8fb7778f31bfc34037de0c65e80
JanVan01 <j_vanz01@uni-muenster.de>
2016-07-04 17:57:39 +0200

    change labels of chart

--- public/js/zone.js
+++ public/js/zone.js
@@ -24,7 +24,7 @@ var ctx = document.getElementById("zoneChart");
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels:[1,5,1,1,1,1,3,0,0,0,3,0,2,1,0,2,0,0,1,1,1,2,0,3],
+        labels:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
         datasets: [{
 
             backgroundColor: "rgba(255,99,132,1)",
